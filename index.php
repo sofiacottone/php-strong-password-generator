cf72eafbea5c119173f85ff4ddd485733b155f0f
sofiacottone <cottonesofia@outlook.it>
2024-05-03 17:47:14 +0200

    feat: start session to read psw length in return.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 <?php
 $userPswLength = $_GET['psw-length'] ?? '';
+
+if ($userPswLength > 2) {
+    session_start();
+    $_SESSION['pswLength'] = $userPswLength;
+    header('Location: ./return.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,18 +29,16 @@ $userPswLength = $_GET['psw-length'] ?? '';
             <h1 class="text-center py-3">Strong Password Generator</h1>
 
             <form method="GET">
-                <div class="hstack gap-3">
+                <div class="hstack justify-content-center gap-3">
                     <label for="psw-length" class="form-label mb-0">Password length:</label>
                     <input type="number" name="psw-length" id="psw-length" min="2" value="<?php echo $userPswLength ?>" class="form-control w-auto">
 
                     <button type="submit" class="btn btn-light">Generate</button>
                 </div>
             </form>
-
-            <div class="my-3">Your Password: <?php echo generatePassword($userPswLength); ?></div>
-
         </div>
     </div>
 </body>
+<?php session_destroy(); ?>
 
 </html>
